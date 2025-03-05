e3bfda141c0ddebee3980061e748f854683419af
Nick <nick@nmartindev.com>
2025-03-05 01:13:17 -0700

    Normalize line endings

--- Radness.Api/Filters/ValidationFilters.cs
+++
@@ -1,7 +0,0 @@
-namespace Radness.Api.Filters;
-
-public static class ValidationFiltersExtensions 
-{
-    private static readonly ProducesResponseTypeMetadata ValidationErrorResponseMetadata = 
-        new(400, typeof(HttpValidationProblemDetails), ["application/problem+json"]);
-}
