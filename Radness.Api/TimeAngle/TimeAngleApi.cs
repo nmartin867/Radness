98c141794b080cafa255b899d024b74ac04b8991
Nick <nick@nmartindev.com>
2025-03-05 01:43:51 -0700

    Update Project to target .net9.0

--- Radness.Api/TimeAngle/TimeAngleApi.cs
+++ Radness.Api/TimeAngle/TimeAngleApi.cs
@@ -2,13 +2,10 @@ namespace Radness.Api.Endpoints;
 
 internal static class TimeAngleApi
 {
-   public static RouteGroupBuilder MapTimeAngles(this IEndpointRouteBuilder routes)
-   {
-      var group = routes.MapGroup("/timeangles");
+    public static RouteGroupBuilder MapTimeAngles(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/angles");
 
-      group.MapGet("/",  context =>
-      {
-         
-      });
-   }
+        group.MapGet("/", context => { });
+    }
 }
